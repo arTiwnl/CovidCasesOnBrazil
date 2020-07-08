61e5c3f0b32762ebfe08837d46692eee5c92ff51
arTiwnl <arTiwnl@users.noreply.github.com>
2020-07-07 21:41:56 -0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,10 @@
+$.getJSON ('https://coronavirus-19-api.herokuapp.com/countries/brazil', function (data){
+    console.log(data);
+    var totaly = data.cases;
+    var recovery = data.recovered;
+    var deaths = data.deaths;
+
+    $(".totaly").append(totaly);
+    $(".recovery").append(recovery);
+    $(".deaths").append(deaths);
+});
